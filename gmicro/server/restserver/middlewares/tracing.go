aa06ec7358b70368acb5884d89536b1a30553ae6
tlq <tanglanqin@gmail.com>
2026-08-18 08:54:15 +0800

    1.优化Prometheus弹性超时 2.过滤心跳,链路的access log 和 HTTP trace,保留审计记录 3.安全响应头新增默认 CSP 和 HSTS

--- gmicro/server/restserver/middlewares/tracing.go
+++ gmicro/server/restserver/middlewares/tracing.go
@@ -1,10 +1,14 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func TracingHandler(service string) gin.HandlerFunc {
-	return otelgin.Middleware(service)
+	return otelgin.Middleware(service, otelgin.WithFilter(func(req *http.Request) bool {
+		return !isLowNoiseManagementPath(req.URL.Path)
+	}))
 }
